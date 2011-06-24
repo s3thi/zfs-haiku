9368cef523c5338f5e5aceb6fb6588132e46823a
Ankur Sethi <get.me.ankur@gmail.com>
2011-06-24 12:42:30 +0000

    create file.h and uio.h

---
+++ src/lib/libsolcompat/include/sys/file.h
@@ -0,0 +1,9 @@
+#ifndef _SOL_SYS_FILE_H
+#define _SOL_SYS_FILE_H
+
+#include_next <sys/file.h>
+
+#define FWRITE  2
+
+#endif /* _SOL_SYS_FILE_H */
+
---
+++ src/lib/libsolcompat/include/sys/uio.h
@@ -0,0 +1,10 @@
+#ifndef _SOL_SYS_UIO_H
+#define _SOL_SYS_UIO_H
+
+#include <sys/types.h>
+#include <sys/feature_tests.h>
+#include_next <sys/uio.h>
+typedef struct iovec iovec_t;
+
+#endif /* _SOL_SYS_UIO_H */
+
