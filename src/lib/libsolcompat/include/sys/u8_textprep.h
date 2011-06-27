ddc9b9b28f89ec1e445e07200060ad3528bfd71f
Ankur Sethi <get.me.ankur@gmail.com>
2011-06-27 12:54:58 +0000

    define U8_TEXTPREP_TOUPPER. this fixes dsl_dataset.c.

---
+++ src/lib/libsolcompat/include/sys/u8_textprep.h
@@ -0,0 +1,8 @@
+#ifndef _SOL_SYS_U8_TEXTPREP_H
+#define _SOL_SYS_U8_TEXTPREP_H
+
+#define U8_STRCMP_CI_UPPER      (0x00000002)
+#define U8_TEXTPREP_TOUPPER     (U8_STRCMP_CI_UPPER)
+
+#endif /* _SOL_SYS_U8_TEXTPREP_H */
+
