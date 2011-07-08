e47af601b5cd00b714dc66fb6bab906db30b9052
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-08 14:35:34 +0000

    import libxdr.

---
+++ src/lib/libxdr/include/mt.h
@@ -0,0 +1,6 @@
+#ifndef _SOL_LIBNSL_MT_H
+#define _SOL_LIBNSL_MT_H
+
+#include <thread.h>
+
+#endif /* _SOL_LIBNSL_MT_H */
---
+++ src/lib/libxdr/include/rpc_mt.h
@@ -0,0 +1,7 @@
+#ifndef _SOL_LIBNSL_RPC_MT_H
+#define _SOL_LIBNSL_RPC_MT_H
+
+#include <sys/types.h>
+#include <rpc/rpc.h>
+
+#endif /* _SOL_LIBNSL_RPC_MT_H */
