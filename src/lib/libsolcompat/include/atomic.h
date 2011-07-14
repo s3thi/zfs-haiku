011b736f9ee7bad94af33819debaac98bdd205e9
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-14 19:44:56 +0000

    define membar_producer. as with all other atomic functions, this needs review.

--- src/lib/libsolcompat/include/atomic.h
+++ src/lib/libsolcompat/include/atomic.h
@@ -32,4 +32,6 @@ uint32_t atomic_cas_32(volatile uint32_t *, uint32_t, uint32_t);
 uint64_t atomic_cas_64(volatile uint64_t *, uint64_t, uint64_t);
 void *atomic_cas_ptr(volatile void *, void *, void *);
 
+#define membar_producer() __asm__ __volatile__ ("sfence");
+
 #endif /* _SOL_SYS_ATOMIC_H */
