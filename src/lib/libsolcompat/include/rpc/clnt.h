c91f68d7e7bf8c89e8cc6e98cf4fb8652e610982
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-15 01:34:23 +0000

    import more routines from libnsl. i should write test programs for part of libxdr.

--- src/lib/libsolcompat/include/rpc/clnt.h
+++ src/lib/libsolcompat/include/rpc/clnt.h
@@ -33,4 +33,37 @@ struct rpc_createerr {
 	struct rpc_err cf_error; /* useful when cf_stat == RPC_PMAPFAILURE */
 };
 
+
+/*
+ * Client rpc handle.
+ * Created by individual implementations, see e.g. rpc_udp.c.
+ * Client is responsible for initializing auth, see e.g. auth_none.c.
+ */
+typedef struct {
+	AUTH	*cl_auth;			/* authenticator */
+	struct clnt_ops {
+		enum clnt_stat	(*cl_call)();	/* call remote procedure */
+		void		(*cl_abort)();	/* abort a call */
+		void		(*cl_geterr)();	/* get specific error code */
+		bool_t		(*cl_freeres)(); /* frees results */
+		void		(*cl_destroy)(); /* destroy this structure */
+		bool_t		(*cl_control)(); /* the ioctl() of rpc */
+	} *cl_ops;
+	caddr_t			cl_private;	/* private stuff */
+} CLIENT;
+
+#define CLSET_FD_NCLOSE 9
+#define	CLNT_DESTROY(rh)	((*(rh)->cl_ops->cl_destroy)(rh))
+#define	clnt_destroy(rh)	((*(rh)->cl_ops->cl_destroy)(rh))
+
+/*
+ * bool_t
+ * CLNT_CONTROL(cl, request, info)
+ *	CLIENT *cl;
+ *	u_int request;
+ *	char *info;
+ */
+#define	CLNT_CONTROL(cl, rq, in) ((*(cl)->cl_ops->cl_control)(cl, rq, in))
+#define	clnt_control(cl, rq, in) ((*(cl)->cl_ops->cl_control)(cl, rq, in))
+
 #endif /* _SOL_RPC_CLNT_H */
---
+++ src/lib/libsolcompat/include/sys/tiuser.h
@@ -0,0 +1,14 @@
+#ifndef _SOL_SYS_TIUSER_H
+#define _SOL_SYS_TIUSER_H
+
+/*
+ * netbuf structure
+ */
+
+struct netbuf {
+	unsigned int	maxlen;
+	unsigned int	len;
+	char		*buf;
+};
+
+#endif /* _SOL_SYS_TIUSER_H */
