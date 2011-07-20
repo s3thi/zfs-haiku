8cd5aa86203d2463ead53360e6f6934163e292a9
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-20 12:39:45 +0000

    define extmnttab.

--- src/lib/libsolcompat/include/sys/mnttab.h
+++ src/lib/libsolcompat/include/sys/mnttab.h
@@ -11,4 +11,19 @@ struct mnttab {
 	char	*mnt_time;
 };
 
+/*
+ * NOTE: fields in extmnttab should match struct mnttab till new fields
+ * are encountered, this allows hasmntopt to work properly when its arg is
+ * a pointer to an extmnttab struct cast to a mnttab struct pointer.
+ */
+
+struct extmnttab {
+	char *mnt_special;
+	char *mnt_mountp;
+	char *mnt_fstype;
+	char *mnt_mntopts;
+	uint_t mnt_major;
+	uint_t mnt_minor;
+};
+
 #endif /* _SOL_SYS_MNTTAB_H */
