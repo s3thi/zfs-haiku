9711930b6d112a652a5c2eec02a21279115915e6
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-20 20:31:20 +0000

    import zoneid functions from zfs-fuse. we assume that there is only one global zone.

--- src/lib/libsolcompat/include/zone.h
+++ src/lib/libsolcompat/include/zone.h
@@ -1,7 +1,13 @@
 #ifndef _SOL_SYS_ZONEID_H
 #define _SOL_SYS_ZONEID_H
 
+#include <sys/types.h>
+
 #define GLOBAL_ZONEID 0
 #define GLOBAL_ZONEID_NAME "global"
+
+zoneid_t getzoneid();
+zoneid_t getzoneidbyname(const char *name);
+ssize_t getzonenamebyid(zoneid_t id, char *buf, size_t buflen);
 
 #endif /* _SOL_SYS_ZONEID_H */
