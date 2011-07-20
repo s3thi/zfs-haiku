3fec4aa2aeab00e38ecba164135d3ef23dd07cd2
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-20 13:31:23 +0000

    define some constants.

--- src/lib/libsolcompat/include/libshare.h
+++ src/lib/libsolcompat/include/libshare.h
@@ -4,6 +4,9 @@
 typedef void *sa_share_t;
 typedef void *sa_handle_t;
 
+#define SA_OK 0
 #define SA_CONFIG_ERR 6
+
+#define SA_INIT_CONTROL_API 0x0002 /* init control specific interface */
 
 #endif /* _SOL_LIBSHARE_H */
