4f4c177bec2105ba131cdac38e3d99bfe5e5a70f
Ankur Sethi <get.me.ankur@gmail.com>
2011-07-22 20:05:23 +0000

    import statcommon.h for zpool.

---
+++ src/lib/libsolcompat/include/statcommon.h
@@ -0,0 +1,8 @@
+#ifndef _SOL_STATCOMMON_H
+#define _SOL_STATCOMMON_H
+
+#define NODATE 0    /* Default:  No time stamp */
+#define DDATE  1    /* Standard date format */
+#define UDATE  2    /* Internal representation of Unix time */
+
+#endif /* _SOL_STATCOMMON_H */
