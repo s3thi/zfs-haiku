f2d558a19dbd7a551b37fbed48a11a2f403108d1
Ankur Sethi <get.me.ankur@gmail.com>
2011-08-10 13:16:08 +0000

    multiply the microseconds returned by system_time() by 1000.

--- src/lib/libsolcompat/time.c
+++ src/lib/libsolcompat/time.c
@@ -1,9 +1,12 @@
 #include <sys/time.h>
 
-// zfs-haiku: we don't have nanosecond resolution, so this
-// will have to do for now.
+/* zfs-haiku: we don't have nanosecond resolution, so this
+ * will have to do for now. We need to multiply by 1000 because
+ * a lot of code is dividing the value returned by this routine
+ * by the NANOSEC constant.
+ */
 hrtime_t
 gethrtime()
 {
-	return (hrtime_t)system_time();
+	return (hrtime_t)(system_time() * 1000);
 }
