6c6c02c78e795e20ed367b9937a00b31bf419f95
Ankur Sethi <get.me.ankur@gmail.com>
2011-08-11 21:18:02 +0000

    fix gethrtime() implementation.

--- src/lib/libsolcompat/time.cpp
+++ src/lib/libsolcompat/time.cpp
@@ -1,12 +1,15 @@
 #include <sys/time.h>
+#include <OS.h>
 
 /* zfs-haiku: we don't have nanosecond resolution, so this
  * will have to do for now. We need to multiply by 1000 because
  * a lot of code is dividing the value returned by this routine
  * by the NANOSEC constant.
  */
-hrtime_t
+extern "C" hrtime_t
 gethrtime()
 {
-	return (hrtime_t)(system_time() * 1000);
+	hrtime_t time = (hrtime_t) system_time();
+	time *= 1000;
+	return time;
 }
