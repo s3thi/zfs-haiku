1f8e099526c03bb73e2d77778aec0625d58c5f2d
Ankur Sethi <get.me.ankur@gmail.com>
2011-08-11 21:16:57 +0000

    hrtime_t is now a bigtime_t.

--- src/lib/libsolcompat/include/sys/time_impl.h
+++ src/lib/libsolcompat/include/sys/time_impl.h
@@ -2,10 +2,11 @@
 #define	_SYS_TIME_IMPL_H
 
 #include <sys/types.h>
+#include <support/SupportDefs.h>
 
 typedef struct timespec timestruc_t;
 typedef struct timespec timespec_t;
-typedef longlong_t hrtime_t;
+typedef bigtime_t hrtime_t;
 
 #endif	/* _SYS_TIME_IMPL_H */
 
