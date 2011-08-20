c72eaad7e9ec209bea6d972fd9d04488f282d890
Ankur Sethi <get.me.ankur@gmail.com>
2011-08-20 11:06:41 +0000

    add mount.cpp for mount() and umount2().

---
+++ src/lib/libsolcompat/mount.cpp
@@ -0,0 +1,20 @@
+extern "C" {
+
+/*
+int
+umount2(const char *file, int mflag)
+{
+}
+
+int mount(const char *spec, const char *dir, int mflag,
+		  char *fstype, char *dataptr,int datalen, char *optptr,
+		  int optlen)
+{
+	volume = fs_mount_volume(mountPoint, device, fs, flags, parameter);
+	if (volume < B_OK) {
+		fprintf(stderr, "%s: %s\n", programName, strerror(volume));
+		return 1;
+	}
+}
+*/
+} /* extern "C" */
